28e6723f421f37dbfa61a04098aa05ebdd453b53
iv-ov <iv-ov@users.noreply.github.com>
2020-04-15 18:19:29 +0300

    Added "details" functionality and `children` prop to SplashError

--- src/components/SplashError/index.js
+++ src/components/SplashError/index.js
@@ -1,24 +1,61 @@
 // @flow
 import * as React from 'react';
-import { NonIdealState } from '../NonIdealState';
+import { NonIdealState, NonIdealStateAction } from '../NonIdealState';
 import image from '../../images/window-shocked.svg';
+import { Button } from '../Button'
+import { Modal } from '../Modal';
 
 
 type Props = {
   title: string,
   description: string,
+  details?: React.Node,
+  children?: React.Node,
 };
 
 export const SplashError = (
   {
     description,
-    title
+    title,
+    details,
+    children
   }: Props
-) => (
-  <NonIdealState
-    isError
-    image={image}
-    title={title}
-    description={description}
-  />
-);
+) => {
+  const [showDetails, setShowDetails] = React.useState(false);
+  const onCloseClick = () => setShowDetails(false);
+
+  const commonProps = {
+    isError: true,
+    image,
+    title,
+    description,
+    children
+  };
+  return <>
+    {showDetails &&
+      <Modal
+        title={title}
+        onClose={onCloseClick}
+        footerControls={[
+          <Button intent={'primary'} onClick={onCloseClick}>Close</Button>
+        ]}
+      >
+        {details}
+      </Modal>
+    }
+
+    {details
+      ? (
+        <NonIdealStateAction
+          {...commonProps}
+          actionText="Details"
+          onActionClick={() => setShowDetails(true)}
+        />
+      ) : (
+        <NonIdealState
+          {...commonProps}
+        />
+      )
+    }
+  </>;
+}
