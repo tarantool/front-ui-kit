b3d8922a5002599f266fae4d54b7fecfabbff511
Roman Babaev <r.babaev@corp.mail.ru>
2020-06-04 03:58:14 +0300

    Add test & update readme

---
+++ src/components/Table/test.js
@@ -0,0 +1,147 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Button } from '../Button';
+import { Table } from './index';
+
+const columns = React.useMemo(
+  () => [
+    {
+      Header: 'First Name',
+      accessor: 'firstName',
+      disableSortBy: true
+    },
+    {
+      Header: 'Last Name',
+      accessor: 'lastName',
+      disableSortBy: true
+    },
+    {
+      Header: 'Age',
+      accessor: 'age',
+      disableSortBy: true
+    },
+    {
+      Header: 'Visits',
+      accessor: 'visits',
+      disableSortBy: true
+    },
+    {
+      Header: 'Status',
+      accessor: 'status',
+      disableSortBy: true
+    },
+    {
+      Header: 'Profile Progress',
+      accessor: 'progress'
+    }
+  ],
+  []
+);
+
+describe('Table', () => {
+  it('renders empty', () => {
+    const tree = renderer.create(
+      <>
+        <Table columns={columns} data={[]} />
+        <Table columns={columns} />
+      </>
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders simple', () => {
+    const data = React.useMemo(
+      () => [
+        {
+          firstName: 'spiders',
+          lastName: 'profession',
+          age: 15,
+          visits: 55,
+          progress: 66,
+          status: 'complicated'
+        },
+        {
+          firstName: 'spiders',
+          lastName: 'profession',
+          age: 15,
+          visits: 55,
+          progress: 66,
+          status: 'complicated'
+        },
+        {
+          firstName: 'profit',
+          lastName: 'quiet',
+          age: 8,
+          visits: 79,
+          progress: 9,
+          status: 'complicated'
+        },
+        {
+          firstName: 'hat',
+          lastName: 'jelly',
+          age: 7,
+          visits: 56,
+          progress: 67,
+          status: <Button text='Refresh' size='xs' />
+        }
+      ],
+      []
+    );
+
+    const tree = renderer.create(
+      <Table columns={columns} data={data} />
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders with code row', () => {
+    const data = React.useMemo(
+      () => [
+        {
+          firstName: 'spiders',
+          lastName: 'profession',
+          age: 15,
+          visits: 55,
+          progress: 66,
+          status: 'complicated',
+          code: 'some code1'
+        },
+        {
+          firstName: 'spiders',
+          lastName: 'profession',
+          age: 15,
+          visits: 55,
+          progress: 66,
+          status: 'complicated',
+          code: 'some code2'
+        },
+        {
+          firstName: 'profit',
+          lastName: 'quiet',
+          age: 8,
+          visits: 79,
+          progress: 9,
+          status: 'complicated'
+        },
+        {
+          firstName: 'hat',
+          lastName: 'jelly',
+          age: 7,
+          visits: 56,
+          progress: 67,
+          status: 'relationship',
+          code: 'some code4'
+        }
+      ],
+      []
+    );
+
+    const tree = renderer.create(
+      <Table columns={columns} data={data} codeRowKey='code' />
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
