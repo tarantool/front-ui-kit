ec28b9bc6c55240b9e0a3dd53f22c324d43bef22
Roman Babaev <r.babaev@corp.mail.ru>
2020-08-28 11:38:46 +0300

    Update tests

--- src/components/Button/test.js
+++ src/components/Button/test.js
@@ -8,25 +8,37 @@ describe('Simple buttons renders correctly', () => {
     const tree = renderer.create(
       <>
         <Button title='Click me right meow!'>Click me</Button>
+        <Button title='Click me right meow!' disabled>Click me</Button>
         <Button intent='primary'>Click me</Button>
+        <Button intent='primary' disabled>Click me</Button>
         <Button intent='secondary'>Click me</Button>
-        <Button intent='iconic'>Click me</Button>
+        <Button intent='secondary' disabled>Click me</Button>
         <Button intent='plain'>Click me</Button>
-        <Button disabled>Click me</Button>
+        <Button intent='plain' disabled>Click me</Button>
+        <Button intent='dark'>Click me</Button>
+        <Button intent='dark' disabled>Click me</Button>
 
         <Button size='s' title='Click me right meow!'>Click me</Button>
+        <Button size='s' title='Click me right meow!' disabled>Click me</Button>
         <Button size='s' intent='primary'>Click me</Button>
+        <Button size='s' intent='primary' disabled>Click me</Button>
         <Button size='s' intent='secondary'>Click me</Button>
-        <Button size='s' intent='iconic'>Click me</Button>
+        <Button size='s' intent='secondary' disabled>Click me</Button>
         <Button size='s' intent='plain'>Click me</Button>
-        <Button size='s' disabled>Click me</Button>
+        <Button size='s' intent='plain' disabled>Click me</Button>
+        <Button size='s' intent='dark'>Click me</Button>
+        <Button size='s' intent='dark' disabled>Click me</Button>
 
         <Button size='xs' title='Click me right meow!'>Click me</Button>
+        <Button size='xs' title='Click me right meow!' disabled>Click me</Button>
         <Button size='xs' intent='primary'>Click me</Button>
+        <Button size='xs' intent='primary' disabled>Click me</Button>
         <Button size='xs' intent='secondary'>Click me</Button>
-        <Button size='xs' intent='iconic'>Click me</Button>
+        <Button size='xs' intent='secondary' disabled>Click me</Button>
         <Button size='xs' intent='plain'>Click me</Button>
-        <Button size='xs' disabled>Click me</Button>
+        <Button size='xs' intent='plain' disabled>Click me</Button>
+        <Button size='xs' intent='dark'>Click me</Button>
+        <Button size='xs' intent='dark' disabled>Click me</Button>
       </>
     ).toJSON();
 
@@ -37,25 +49,37 @@ describe('Simple buttons renders correctly', () => {
     const tree = renderer.create(
       <>
         <Button loading title='Click me right meow!'>Click me</Button>
+        <Button loading title='Click me right meow!' disabled>Click me</Button>
         <Button loading intent='primary'>Click me</Button>
+        <Button loading intent='primary' disabled>Click me</Button>
         <Button loading intent='secondary'>Click me</Button>
-        <Button loading intent='iconic'>Click me</Button>
+        <Button loading intent='secondary' disabled>Click me</Button>
         <Button loading intent='plain'>Click me</Button>
-        <Button loading disabled>Click me</Button>
+        <Button loading intent='plain' disabled>Click me</Button>
+        <Button loading intent='dark'>Click me</Button>
+        <Button loading intent='dark' disabled>Click me</Button>
 
         <Button loading size='s' title='Click me right meow!'>Click me</Button>
+        <Button loading size='s' title='Click me right meow!' disabled>Click me</Button>
         <Button loading size='s' intent='primary'>Click me</Button>
+        <Button loading size='s' intent='primary' disabled>Click me</Button>
         <Button loading size='s' intent='secondary'>Click me</Button>
-        <Button loading size='s' intent='iconic'>Click me</Button>
+        <Button loading size='s' intent='secondary' disabled>Click me</Button>
         <Button loading size='s' intent='plain'>Click me</Button>
-        <Button loading size='s' disabled>Click me</Button>
+        <Button loading size='s' intent='plain' disabled>Click me</Button>
+        <Button loading size='s' intent='dark'>Click me</Button>
+        <Button loading size='s' intent='dark' disabled>Click me</Button>
 
         <Button loading size='xs' title='Click me right meow!'>Click me</Button>
+        <Button loading size='xs' title='Click me right meow!' disabled>Click me</Button>
         <Button loading size='xs' intent='primary'>Click me</Button>
+        <Button loading size='xs' intent='primary' disabled>Click me</Button>
         <Button loading size='xs' intent='secondary'>Click me</Button>
-        <Button loading size='xs' intent='iconic'>Click me</Button>
+        <Button loading size='xs' intent='secondary' disabled>Click me</Button>
         <Button loading size='xs' intent='plain'>Click me</Button>
-        <Button loading size='xs' disabled>Click me</Button>
+        <Button loading size='xs' intent='plain' disabled>Click me</Button>
+        <Button loading size='xs' intent='dark'>Click me</Button>
+        <Button loading size='xs' intent='dark' disabled>Click me</Button>
       </>
     ).toJSON();
 
@@ -68,46 +92,70 @@ describe('Buttons with icons renders correctly', () => {
     const tree = renderer.create(
       <>
         <Button icon={IconOk} title='Click me right meow!'>Click me</Button>
+        <Button icon={IconOk} title='Click me right meow!' disabled>Click me</Button>
         <Button icon={IconOk} intent='primary'>Click me</Button>
+        <Button icon={IconOk} intent='primary' disabled>Click me</Button>
         <Button icon={IconOk} intent='secondary'>Click me</Button>
-        <Button icon={IconOk} intent='iconic'>Click me</Button>
+        <Button icon={IconOk} intent='secondary' disabled>Click me</Button>
         <Button icon={IconOk} intent='plain'>Click me</Button>
-        <Button icon={IconOk} disabled>Click me</Button>
+        <Button icon={IconOk} intent='plain' disabled>Click me</Button>
+        <Button icon={IconOk} intent='dark'>Click me</Button>
+        <Button icon={IconOk} intent='dark' disabled>Click me</Button>
 
         <Button icon={IconOk} size='s' title='Click me right meow!'>Click me</Button>
+        <Button icon={IconOk} size='s' title='Click me right meow!' disabled>Click me</Button>
         <Button icon={IconOk} size='s' intent='primary'>Click me</Button>
+        <Button icon={IconOk} size='s' intent='primary' disabled>Click me</Button>
         <Button icon={IconOk} size='s' intent='secondary'>Click me</Button>
-        <Button icon={IconOk} size='s' intent='iconic'>Click me</Button>
+        <Button icon={IconOk} size='s' intent='secondary' disabled>Click me</Button>
         <Button icon={IconOk} size='s' intent='plain'>Click me</Button>
-        <Button icon={IconOk} size='s' disabled>Click me</Button>
+        <Button icon={IconOk} size='s' intent='plain' disabled>Click me</Button>
+        <Button icon={IconOk} size='s' intent='dark'>Click me</Button>
+        <Button icon={IconOk} size='s' intent='dark' disabled>Click me</Button>
 
         <Button iconRight={IconOk} title='Click me right meow!'>Click me</Button>
+        <Button iconRight={IconOk} title='Click me right meow!' disabled>Click me</Button>
         <Button iconRight={IconOk} intent='primary'>Click me</Button>
+        <Button iconRight={IconOk} intent='primary' disabled>Click me</Button>
         <Button iconRight={IconOk} intent='secondary'>Click me</Button>
-        <Button iconRight={IconOk} intent='iconic'>Click me</Button>
+        <Button iconRight={IconOk} intent='secondary' disabled>Click me</Button>
         <Button iconRight={IconOk} intent='plain'>Click me</Button>
-        <Button iconRight={IconOk} disabled>Click me</Button>
+        <Button iconRight={IconOk} intent='plain' disabled>Click me</Button>
+        <Button iconRight={IconOk} intent='dark'>Click me</Button>
+        <Button iconRight={IconOk} intent='dark' disabled>Click me</Button>
 
         <Button iconRight={IconOk} size='s' title='Click me right meow!'>Click me</Button>
+        <Button iconRight={IconOk} size='s' title='Click me right meow!' disabled>Click me</Button>
         <Button iconRight={IconOk} size='s' intent='primary'>Click me</Button>
+        <Button iconRight={IconOk} size='s' intent='primary' disabled>Click me</Button>
         <Button iconRight={IconOk} size='s' intent='secondary'>Click me</Button>
-        <Button iconRight={IconOk} size='s' intent='iconic'>Click me</Button>
+        <Button iconRight={IconOk} size='s' intent='secondary' disabled>Click me</Button>
         <Button iconRight={IconOk} size='s' intent='plain'>Click me</Button>
-        <Button iconRight={IconOk} size='s' disabled>Click me</Button>
+        <Button iconRight={IconOk} size='s' intent='plain' disabled>Click me</Button>
+        <Button iconRight={IconOk} size='s' intent='dark'>Click me</Button>
+        <Button iconRight={IconOk} size='s' intent='dark' disabled>Click me</Button>
 
         <Button icon={IconOk} iconRight={IconOk} title='Click me right meow!'>Click me</Button>
+        <Button icon={IconOk} iconRight={IconOk} title='Click me right meow!' disabled>Click me</Button>
         <Button icon={IconOk} iconRight={IconOk} intent='primary'>Click me</Button>
+        <Button icon={IconOk} iconRight={IconOk} intent='primary' disabled>Click me</Button>
         <Button icon={IconOk} iconRight={IconOk} intent='secondary'>Click me</Button>
-        <Button icon={IconOk} iconRight={IconOk} intent='iconic'>Click me</Button>
+        <Button icon={IconOk} iconRight={IconOk} intent='secondary' disabled>Click me</Button>
         <Button icon={IconOk} iconRight={IconOk} intent='plain'>Click me</Button>
-        <Button icon={IconOk} iconRight={IconOk} disabled>Click me</Button>
+        <Button icon={IconOk} iconRight={IconOk} intent='plain' disabled>Click me</Button>
+        <Button icon={IconOk} iconRight={IconOk} intent='dark'>Click me</Button>
+        <Button icon={IconOk} iconRight={IconOk} intent='dark' disabled>Click me</Button>
 
         <Button icon={IconOk} iconRight={IconOk} size='s' title='Click me right meow!'>Click me</Button>
+        <Button icon={IconOk} iconRight={IconOk} size='s' title='Click me right meow!' disabled>Click me</Button>
         <Button icon={IconOk} iconRight={IconOk} size='s' intent='primary'>Click me</Button>
+        <Button icon={IconOk} iconRight={IconOk} size='s' intent='primary' disabled>Click me</Button>
         <Button icon={IconOk} iconRight={IconOk} size='s' intent='secondary'>Click me</Button>
-        <Button icon={IconOk} iconRight={IconOk} size='s' intent='iconic'>Click me</Button>
+        <Button icon={IconOk} iconRight={IconOk} size='s' intent='secondary' disabled>Click me</Button>
         <Button icon={IconOk} iconRight={IconOk} size='s' intent='plain'>Click me</Button>
-        <Button icon={IconOk} iconRight={IconOk} size='s' disabled>Click me</Button>
+        <Button icon={IconOk} iconRight={IconOk} size='s' intent='plain' disabled>Click me</Button>
+        <Button icon={IconOk} iconRight={IconOk} size='s' intent='dark'>Click me</Button>
+        <Button icon={IconOk} iconRight={IconOk} size='s' intent='dark' disabled>Click me</Button>
       </>
     ).toJSON();
 
@@ -118,18 +166,26 @@ describe('Buttons with icons renders correctly', () => {
     const tree = renderer.create(
       <>
         <Button icon={IconOk} loading title='Click me right meow!'>Click me</Button>
+        <Button icon={IconOk} loading title='Click me right meow!' disabled>Click me</Button>
         <Button icon={IconOk} loading intent='primary'>Click me</Button>
+        <Button icon={IconOk} loading intent='primary' disabled>Click me</Button>
         <Button icon={IconOk} loading intent='secondary'>Click me</Button>
-        <Button icon={IconOk} loading intent='iconic'>Click me</Button>
+        <Button icon={IconOk} loading intent='secondary' disabled>Click me</Button>
         <Button icon={IconOk} loading intent='plain'>Click me</Button>
-        <Button icon={IconOk} loading disabled>Click me</Button>
+        <Button icon={IconOk} loading intent='plain' disabled>Click me</Button>
+        <Button icon={IconOk} loading intent='dark'>Click me</Button>
+        <Button icon={IconOk} loading intent='dark' disabled>Click me</Button>
 
         <Button icon={IconOk} loading size='s' title='Click me right meow!'>Click me</Button>
+        <Button icon={IconOk} loading size='s' title='Click me right meow!' disabled>Click me</Button>
         <Button icon={IconOk} loading size='s' intent='primary'>Click me</Button>
+        <Button icon={IconOk} loading size='s' intent='primary' disabled>Click me</Button>
         <Button icon={IconOk} loading size='s' intent='secondary'>Click me</Button>
-        <Button icon={IconOk} loading size='s' intent='iconic'>Click me</Button>
+        <Button icon={IconOk} loading size='s' intent='secondary' disabled>Click me</Button>
         <Button icon={IconOk} loading size='s' intent='plain'>Click me</Button>
-        <Button icon={IconOk} loading size='s' disabled>Click me</Button>
+        <Button icon={IconOk} loading size='s' intent='plain' disabled>Click me</Button>
+        <Button icon={IconOk} loading size='s' intent='dark'>Click me</Button>
+        <Button icon={IconOk} loading size='s' intent='dark' disabled>Click me</Button>
       </>
     ).toJSON();
 
