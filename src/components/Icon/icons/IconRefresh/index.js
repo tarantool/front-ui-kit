14f1799fc676e04976907bb5b03d473fb296b8ae
Roman Babaev <kotfly@gmail.com>
2020-12-08 03:04:33 +0300

    Update IconRefresh (#176)

--- src/components/Icon/icons/IconRefresh/index.js
+++ src/components/Icon/icons/IconRefresh/index.js
@@ -3,12 +3,12 @@ import * as React from 'react';
 import { css, cx } from 'emotion';
 import { Icon, type GenericIconProps } from '../../Icon';
 import { colors } from '../../../../variables';
-import image from './refresh.svg';
+import image from './icon-refresh.svg';
 
 const styles = css`
-  width: 12px;
-  height: 12px;
-  fill: ${colors.intentPrimary45};
+  width: 16px;
+  height: 16px;
+  fill: ${colors.dark65};
 `;
 
 export const IconRefresh = ({ className }: GenericIconProps) => (
