c7a76de06272a817e3cdd5be616cfffb59c3c0fb
Roman Babaev <r.babaev@corp.mail.ru>
2020-12-22 18:08:59 +0300

    WIP: Adds native scrollbars styles

---
+++ src/globalStyles/scrollbar.js
@@ -0,0 +1,30 @@
+// @flow
+import { css } from 'emotion';
+import { rgba } from 'emotion-rgba';
+import { getOS } from '../utils/getOS';
+import { colors } from '../variables';
+
+export const scrollbars = css`
+  & {
+    scrollbar-width: thin;
+    scrollbar-color: ${rgba(colors.dark65, 0.5)} transparent;
+  }
+
+  &::-webkit-scrollbar {
+    width: 6px;
+    margin-right: 3px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background-color: transparent;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: ${rgba(colors.dark65, 0.5)};
+    border-radius: 4px;
+  }
+`;
+
+const isScrollbarStyleRequired = ['Mac OS', 'iOS', 'Android'].indexOf(getOS()) === -1;
+
+export const OSDependScrollbars = isScrollbarStyleRequired ? scrollbars : '';
---
+++ src/utils/getOS.js
@@ -0,0 +1,26 @@
+// @flow
+
+type OS = 'Mac OS' | 'iOS' | 'Windows' | 'Android' | 'Linux' | null;
+
+export function getOS(): OS {
+  var userAgent = window.navigator.userAgent,
+    platform = window.navigator.platform,
+    macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+    windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+    iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+    os = null;
+
+  if (macosPlatforms.indexOf(platform) !== -1) {
+    os = 'Mac OS';
+  } else if (iosPlatforms.indexOf(platform) !== -1) {
+    os = 'iOS';
+  } else if (windowsPlatforms.indexOf(platform) !== -1) {
+    os = 'Windows';
+  } else if (/Android/.test(userAgent)) {
+    os = 'Android';
+  } else if (!os && /Linux/.test(platform)) {
+    os = 'Linux';
+  }
+
+  return os;
+}
