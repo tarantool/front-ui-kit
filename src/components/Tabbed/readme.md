8ae7c56c85f25a83805e9fbc4c3d0b588a6d355b
aShelist <60879129+aShelist@users.noreply.github.com>
2021-07-07 14:28:21 +0300

    Update Tabbed component. Add new prop (#240)
    * Update Tabbed component. Add new prop
    
    * add shadow to tabbed
    
    * update tests
    
    * fix mistake

--- src/components/Tabbed/readme.md
+++ src/components/Tabbed/readme.md
@@ -41,3 +41,29 @@ const [ activeTab, setActiveTab ] = useState(1);
   ]}
 />
 ```
+
+```js
+import { useState } from 'react';
+
+const [ activeTab, setActiveTab ] = useState(1);
+
+<Tabbed
+  activeTab={activeTab}
+  handleTabChange={setActiveTab}
+  size={"small"}
+  tabs={[
+    {
+      label: 'Create Replica Set',
+      content: 'Tab content'
+    },
+    {
+      label: 'Another tab',
+      content: 'Another tab content'
+    },
+    {
+      label: 'Third tab',
+      content: 'Third tab content'
+    }
+  ]}
+/>
+```
--- src/components/Tabbed/test.js
+++ src/components/Tabbed/test.js
@@ -2,25 +2,52 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import { Tabbed } from './index';
 
-it('Tabbed renders correctly', () => {
-  const tree = renderer.create(
-    <Tabbed
-      tabs={[
-        {
-          label: 'Create Replica Set',
-          content: 'Tab content'
-        },
-        {
-          label: 'Another tab',
-          content: 'Another tab content'
-        },
-        {
-          label: 'Third tab',
-          content: 'Third tab content'
-        }
-      ]}
-    />
-  ).toJSON();
+describe('Tabbed', () => {
+  it('Tabbed renders correctly', () => {
+    const tree = renderer.create(
+      <Tabbed
+        tabs={[
+          {
+            label: 'Create Replica Set',
+            content: 'Tab content'
+          },
+          {
+            label: 'Another tab',
+            content: 'Another tab content'
+          },
+          {
+            label: 'Third tab',
+            content: 'Third tab content'
+          }
+        ]}
+      />
+    ).toJSON();
 
-  expect(tree).toMatchSnapshot();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('Tabbed small renders correctly', () => {
+    const tree = renderer.create(
+      <Tabbed
+        size="small"
+        tabs={[
+          {
+            label: 'Create Replica Set',
+            content: 'Tab content'
+          },
+          {
+            label: 'Another tab',
+            content: 'Another tab content'
+          },
+          {
+            label: 'Third tab',
+            content: 'Third tab content'
+          }
+        ]}
+      />
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
 });
+
