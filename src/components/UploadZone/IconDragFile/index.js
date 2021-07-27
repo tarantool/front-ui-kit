3ff9b4963118b68b30792245158cd5dca1b4f8d6
ashelist <a.shelist@corp.mail.ru>
2021-07-27 11:46:57 +0300

    fix lint

--- src/components/UploadZone/IconDragFile/index.js
+++ src/components/UploadZone/IconDragFile/index.js
@@ -2,7 +2,6 @@
 import * as React from 'react';
 import { css, cx } from 'emotion';
 import { Icon, type GenericIconProps } from '../../Icon/Icon';
-import { colors } from '../../../variables';
 import image from './icon-drag-file.svg';
 
 const styles = css`
