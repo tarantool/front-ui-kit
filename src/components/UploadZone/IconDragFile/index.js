2b7061c937365f1c9c85a4abdd7b3c2574376ba1
aShelist <60879129+aShelist@users.noreply.github.com>
2021-07-29 08:10:35 +0300

    Update upload zone (#245)
    * update upload zone component
    
    * update snapshot
    
    * fix lint

---
+++ src/components/UploadZone/IconDragFile/index.js
@@ -0,0 +1,17 @@
+// @flow
+import * as React from 'react';
+import { css, cx } from 'emotion';
+import { Icon, type GenericIconProps } from '../../Icon/Icon';
+import image from './icon-drag-file.svg';
+
+const styles = css`
+  width: 16px;
+  height: 16px;
+`;
+
+export const IconDragFile = ({ className }: GenericIconProps) => (
+  <Icon
+    className={cx(styles, className)}
+    glyph={image}
+  />
+);
