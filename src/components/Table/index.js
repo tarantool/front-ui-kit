21469891b874ae7062970868e0d5bd7876d4ee5a
Timur Usenko <usenko.timur@gmail.com>
2021-08-30 11:01:36 +0000

    Improving the table component and fixing a Safari style problem

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -15,13 +15,16 @@ import { css, cx } from '@emotion/css';
 import { Text } from '../Text';
 import { Spin } from '../Spin';
 import { NonIdealState } from '../NonIdealState';
-import image from '../Icon/icons/IconEmptyData/icon-empty-data.svg';
 import { Pagination, PaginationControlled } from '../Pagination';
 import { Checkbox } from '../Checkbox';
-import { TableRow } from './TableRow';
-import { TableHeader } from './TableHeader';
 import { colors } from '../../variables';
+import image from '../Icon/icons/IconEmptyData/icon-empty-data.svg';
 
+import { TableRow } from './TableRow';
+import { TableStickyRow } from './TableStickyRow';
+import { TableHeader } from './TableHeader';
+
+import type { ManualPagination, AdditionalProps } from './types';
 
 const styles = {
   table: css`
@@ -57,23 +60,7 @@ const styles = {
 };
 
 
-export type RowProps = {
-  rowClassName?: string,
-  topRowClassName?: string,
-  topRowKey?: string,
-  topRowStickySide?: number,
-  onRowClick?: (row: Row) => void;
-}
-
-export type ManualPagination = {
-  page: number,
-  pageSize: number,
-  onChangePage: (page: number) => void,
-  onChangePageSize: (pageSize: number) => void,
-  disableNextPageButton: boolean,
-}
-
-type TableProps = UseTableOptions & RowProps & {
+type TableProps = $Exact<UseTableOptions & AdditionalProps & {
   className?: string,
   pagination?: boolean;
   loading?: boolean;
@@ -82,19 +69,21 @@ type TableProps = UseTableOptions & RowProps & {
   tableKey?: string;
   initialSelectedRowIds?: any[],
   initialSortBy?: Array<{ id: string, desc: boolean }>
-};
+}>;
+
+const emptyArr = [];
 
 export function Table(props: TableProps) {
   const {
     rowClassName,
-    columns = [],
+    columns = emptyArr,
     className,
-    data = [],
+    data = emptyArr,
     pagination,
     onSelectedRowsChange,
     tableKey,
-    initialSelectedRowIds = [],
-    initialSortBy = [],
+    initialSelectedRowIds = emptyArr,
+    initialSortBy = emptyArr,
     manualPagination,
     loading = false
   } = props;
@@ -102,11 +91,8 @@ export function Table(props: TableProps) {
   const getRowId = React.useCallback((row, index) => {
     return tableKey && row[tableKey] ? row[tableKey] : index;
   }, [tableKey]);
-  const [ theadHeight, setTHeadHeight ] = React.useState<number>(0);
-  const theadRef = React.useRef(null);
-  React.useEffect(() => {
-    setTHeadHeight(theadRef.current ? theadRef.current.clientHeight : 0);
-  }, []);
+
+  const headerRef = React.useRef<HTMLTableSectionElement | null>(null);
 
   const {
     getTableProps,
@@ -116,17 +102,23 @@ export function Table(props: TableProps) {
     rows,
     page,
     visibleColumns,
-
     gotoPage,
     setPageSize,
     selectedFlatRows,
-    state: { pageIndex, pageSize, selectedRowIds }
+    state: {
+      pageIndex,
+      pageSize,
+      selectedRowIds
+    }
   } = useTable(
     {
       columns,
       data,
       getRowId,
-      initialState: { selectedRowIds: initialSelectedRowIds, sortBy: initialSortBy },
+      initialState: {
+        selectedRowIds: initialSelectedRowIds,
+        sortBy: initialSortBy
+      },
       manualPagination: !!manualPagination,
       autoResetSelectedRows: !manualPagination,
       autoResetSortBy: !manualPagination
@@ -140,10 +132,7 @@ export function Table(props: TableProps) {
         {
           id: 'selection',
           Header: ({ getToggleAllRowsSelectedProps }) => <Checkbox {...getToggleAllRowsSelectedProps()} />,
-          Cell: ({ row }) => {
-            const { checked } = row.getToggleRowSelectedProps();
-            return <Checkbox checked={checked} />
-          }
+          Cell: ({ row: { getToggleRowSelectedProps } }) => <Checkbox {...getToggleRowSelectedProps()} />
         },
         ...columns
       ])
@@ -157,30 +146,44 @@ export function Table(props: TableProps) {
     }
   }, [selectedFlatRows]);
 
-  const dataRows = pagination ? page : rows;
+  const dataRows: Row[] = React.useMemo(() => {
+    return (pagination ? page : rows).map(item => {
+      prepareRow(item);
+      return item;
+    });
+  }, [prepareRow, pagination, page, rows]);
+
   return (
     <>
       <Spin enable={loading} className={cx({ [styles.loading]: loading })}>
         <table {...getTableProps()} className={cx(styles.table, className)}>
           <TableHeader
-            // ref={theadRef} // TODO: Fix error: Maximum update depth exceeded.
+            ref={headerRef}
             headerGroups={headerGroups}
             dataRows={dataRows}
           />
           <tbody className={styles.tbody} {...getTableBodyProps()}>
             {dataRows.map(row => {
-              prepareRow(row);
+              const topRow = props.topRowKey && row.original[props.topRowKey];
               return (
-                <TableRow
-                  key={row.getRowProps().key}
-                  row={row}
-                  rowClassName={rowClassName}
-                  onRowClick={props.onRowClick}
-                  topRowClassName={props.topRowClassName}
-                  topRowKey={props.topRowKey}
-                  topRowStickySide={props.topRowStickySide}
-                  headHeight={theadHeight}
-                />
+                <React.Fragment key={row.getRowProps().key}>
+                  {topRow && (
+                    <TableStickyRow
+                      cellsLength={row.cells.length}
+                      topRowClassName={props.topRowClassName}
+                      topRowKey={props.topRowKey}
+                      topRowStickySide={props.topRowStickySide}
+                      headerRef={headerRef}
+                    >
+                      {topRow}
+                    </TableStickyRow>
+                  )}
+                  <TableRow
+                    row={row}
+                    rowClassName={rowClassName}
+                    onRowClick={props.onRowClick}
+                  />
+                </React.Fragment>
               )
             })}
             {!rows.length && !loading && (
@@ -194,25 +197,29 @@ export function Table(props: TableProps) {
             )}
           </tbody>
         </table>
-        {pagination && rows.length > 0 && <div className={styles.pagination}>
-          <Pagination
-            page={pageIndex}
-            pageSize={pageSize}
-            items={rows.length}
-            onPageChange={gotoPage}
-            setPageSize={setPageSize}
-            showTotal
-          />
-        </div>}
-        {manualPagination && <div className={styles.pagination}>
-          <PaginationControlled
-            page={manualPagination.page}
-            pageSize={manualPagination.pageSize}
-            disableNextPageButton={manualPagination.disableNextPageButton}
-            onPageChange={manualPagination.onChangePage}
-            setPageSize={manualPagination.onChangePageSize}
-          />
-        </div>}
+        {pagination && rows.length > 0 && (
+          <div className={styles.pagination}>
+            <Pagination
+              page={pageIndex}
+              pageSize={pageSize}
+              items={rows.length}
+              onPageChange={gotoPage}
+              setPageSize={setPageSize}
+              showTotal
+            />
+          </div>
+        )}
+        {manualPagination && (
+          <div className={styles.pagination}>
+            <PaginationControlled
+              page={manualPagination.page}
+              pageSize={manualPagination.pageSize}
+              disableNextPageButton={manualPagination.disableNextPageButton}
+              onPageChange={manualPagination.onChangePage}
+              setPageSize={manualPagination.onChangePageSize}
+            />
+          </div>
+        )}
       </Spin>
     </>
   );
