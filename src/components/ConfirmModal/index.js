0df59806d48bf9e73fa2aed2f4890811c6ba2a24
Alexsandr Litkevich <alexshow2362@gmail.com>
2022-02-15 16:12:48 +0400

    Added properties cancelText

--- src/components/ConfirmModal/index.js
+++ src/components/ConfirmModal/index.js
@@ -10,18 +10,19 @@ type ConfirmModalProps = ModalProps & {
   onConfirm: Function,
   onCancel: Function,
   confirmText?: string,
+  cancelText?: string,
   confirmPreloader?: boolean,
 };
 
 export const ConfirmModal = (props: ConfirmModalProps) => {
-  const { onConfirm, onCancel, confirmText = 'Ok', confirmPreloader } = props;
+  const { onConfirm, onCancel, confirmText = 'Ok', confirmPreloader, cancelText = 'Cancel' } = props;
   return (
     <Modal
       {...props}
       onClose={onCancel}
       footerControls={[
         <Button key={0} intent="base" size="l" onClick={onCancel}>
-          Cancel
+          {cancelText}
         </Button>,
         <Button key={1} autoFocus intent="primary" size="l" loading={confirmPreloader || false} onClick={onConfirm}>
           {confirmText}
